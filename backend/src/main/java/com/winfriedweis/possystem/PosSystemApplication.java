cf63843d47a160e354625a72a8f7cee515ded4e1
winfriedweis <weis.winfried@icloud.com>
2025-12-16 10:12:06 +0100

    Initial commit

---
+++ backend/src/main/java/com/winfriedweis/possystem/PosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.winfriedweis.possystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/winfriedweis/possystem/PosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winfriedweis.possystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
